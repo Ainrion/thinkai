e10d7cef61bf05da403078b64d067754180ddfcd
Aditya Narayan <46862270+Adi202001@users.noreply.github.com>
2025-03-17 09:26:50 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    // Disable ESLint during production builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
